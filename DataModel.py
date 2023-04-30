24263b04464676d2efa2d88de3012f3ecff2f4fb
jp-hidalgo <jp.hidalgo@uniandes.edu.co>
2023-04-30 13:47:06 -0500

    data model changes

--- DataModel.py
+++ DataModel.py
@@ -3,10 +3,11 @@ from pydantic import BaseModel
 class DataModel(BaseModel):
 
 # Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
-    year: float
-    km_driven: float
-    # COMPLETAR CON LAS VARIABLES EXPLICATIVAS DE LOS DATOS DEL LABORATORIO 2
-
+    title: str
+    rating: int
+    review_text: str
+    location: str
+    hotel: str
 #Esta función retorna los nombres de las columnas correspondientes con el modelo esxportado en joblib.
     def columns(self):
-        return ["year","km_driven", ...]
+        return ["title","rating", "review_text","location","hotel"]
